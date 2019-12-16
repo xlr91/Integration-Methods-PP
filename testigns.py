531150b427ba6ad596dba6321fdc830e8143a718
Emir <emirmuhammad08@yahoo.com>
2019-12-16 14:29:11 +0700

    -basic framework for n-d adaptint

--- testigns.py
+++ testigns.py
@@ -50,7 +50,80 @@ if kind == 0: #deals with the cmidpoint rule
         X[i] = xnew
         N[i] -= 1
 
+
+def loop_rec(y, n):
+    if n >= 1:
+        for _ in range(y):
+            loop_rec(y, n - 1)
+    else:
+       print(Wlist[y][n])
+
+
+def loopme(I, n, wlistval):
+    wlistval 
+        
+
+
+
+#maybe we can start with value
+N = [2, 3, 4]
+d = 3
+V = [0, 0, 0]
+for x in range(3):
+    V[0] = x
+    for y in range(3):
+        V[1] = y
+        for z in range(3):
+            V[2] = z
+            #print([x, y, z])
+            print(V)
+            #V = []
+
+N = [2, 3, 4]
+d = 3
+V = [0, 0, 0]
+
+def rec(d, V, n = 0, x = None):
+    d -= 1
+    if d >= 1:
+        for x in range(N[d]):
+            V[d] = x
+            #print('x', x)
+            rec(d, V, x)
+    else:
+        for x in range(N[d]):
+            V[d] = x
+            for i in range(len(N)):
+                print(V)
+                thevalue *= Wlist[i][V[i]]
+    
 '''
+#thevalue = f(xi, xj, xk), say its 1 for ease
+thevalue = 1 
+so then what you wanna do is 
+thevalue *= Wlist[0][i]*Wlist[1][j]*Wlist[2][k]*...
+
+so its 
+thevalue *= Wlist[0][i]
+thevalue *= Wlist[1][j]
+thevalue *= Wlist[2][k]
+
+so just for that i can have a 
+
+
+ijk = [0, 0, 0] #this can be easily made
+#just need a way to change them all 
+thevalue = 1
+for o in range(d):
+    thevalue *= Wlist[o][ijk[o]]
+value += thevalue
+
+ijk[0] +=1
+if ijk[0] == something[0]:
+    ijk[0] = 0
+    ijk[0+1] += 1
+
+now i need to find a way to loop over the correct ijks  
 #need to be able to generalize the N for loops
 for i in range(N1):
     for j in range(N2):
